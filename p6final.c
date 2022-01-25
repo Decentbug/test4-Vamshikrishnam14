5f44c85f87ee2bd94c4cd0e29a7f061c3aff3499
Vamshikrishnam1 <93110890+Vamshikrishnam14@users.noreply.github.com>
2022-01-25 06:17:32 +0000

    Committed

---
+++ p6final.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+  int a,b,c;
+  printf("enter 3 numbers");
+  scanf("%d%d%d",&a,&b,&C);
+  if((a,b)&&(a>c))
+  printf("a is &p\n");
+  else
+   if ((b,a)&&(b>c))
+  printf("b is &p\n");
+  else
+  printf("c is &p\n");
+  return 0 ;
+  }
